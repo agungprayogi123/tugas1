7768405edd2448a309419f5f79e6a403b88b1e8c
Gajut Doin Duta Ginting <gajutdoindutagintingbabo@gmail.com>
2025-01-08 17:38:13 +0700

    penambahan pembelian

---
+++ pembelian.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pembelian</title>
+</head>
+<body>
+    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel repellat tempora rem alias esse debitis dolore quod! Et animi voluptatibus quas eveniet reiciendis quod minus, illo consectetur minima placeat est?</h1>
+</body>
+</html>
